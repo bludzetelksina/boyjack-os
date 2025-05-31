6874d3d8cba8fc6b7aa8044c2e522e174f4e992a
bludzetelksina <bludzetelksina@gmail.com>
2025-05-31 16:48:12 +0300

    Create mod.rs

---
+++ userland/lib/mod.rs
@@ -0,0 +1,10 @@
+// userland/lib/mod.rs
+
+pub mod filesystem;
+pub mod io;
+pub mod string_utils;
+pub mod network;
+pub mod config;
+pub mod logger;
+
+// В дальнейшем можно добавить и другие модули
